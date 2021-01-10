9a667f1fdb00be497abb0d7a9485c6221f8d39c9
kenny-kogi <mwangiken497@gmail.com>
2021-01-10 11:52:33 +0300

    factorial recursive function

---
+++ src/com/company/Practise6.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Practise6 {
+
+    // Complete the factorial function below.
+    static  int factorial(int n){
+        if (n == 1){
+            return  1;
+        }else {
+
+            return  n * factorial(n - 1);
+
+        }
+
+
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = factorial(n);
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
