02b27bb2cdff6e4caffc8372071139a97fe8b207
kenny-kogi <mwangiken497@gmail.com>
2021-01-26 23:07:40 +0300

    time complexity

---
+++ src/com/company/Time.java
@@ -0,0 +1,41 @@
+package com.company;
+import java.util.*;
+
+public class Time{
+public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+        numbers[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+        if (isPrime(numbers[i]))
+        System.out.println("Prime");
+        else
+        System.out.println("Not prime");
+        }
+        }
+
+public static boolean isPrime(int number) {
+
+        double numberSqrt = Math.sqrt(number);
+
+        if (number <= 1)
+        return false;
+
+        else {
+        for (int j = 2; j <= numberSqrt; j++) {
+        if (number % j == 0) {
+        return false;
+        }
+        }
+        }
+        return true;
+
+        }
+        }
+
